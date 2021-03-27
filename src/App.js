62e0f99a743cbeb42e0f47b4f1394109d0e8b801
Rami-hlioui <ramihlioui.tsuna@gmail.com>
2021-03-27 17:14:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Component } from "react";
+import Homepage from "./homepage.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
